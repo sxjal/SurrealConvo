dc15c2157931d9e52e1bd3b714b041c84d606509
sajal <sajalsahu8@gmail.com>
2023-09-12 15:14:39 +0530

    constructor of featurebox

--- lib/feature_box.dart
+++ lib/feature_box.dart
@@ -1,10 +1,27 @@
 import 'package:flutter/material.dart';
 
 class FeatureBox extends StatelessWidget {
-  const FeatureBox({super.key});
+  const FeatureBox({
+    super.key,
+    required this.color,
+    required this.title,
+    required this.subtitle,
+  });
+
+  final Color color;
+  final String title;
+  final String subtitle;
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Container(
+      margin: const EdgeInsets.symmetric(
+        horizontal: 35,
+        vertical: 10,
+      ),
+      decoration: BoxDecoration(
+        color: Colors.white,
+      ),
+    );
   }
 }
