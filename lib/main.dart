838dce610bd744ab933c7a829383ccee499f1a45
sajal <sajalsahu8@gmail.com>
2023-09-12 14:21:45 +0530

    homepage

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:surrealconvo/home.dart';
 
 void main() {
   runApp(
     const MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('Hello World'),
+          child: HomePage(),
         ),
       ),
     ),
