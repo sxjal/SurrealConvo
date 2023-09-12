b270ca5d9789f62584873b90923d3663d649fc55
sajal <sajalsahu8@gmail.com>
2023-09-12 14:38:01 +0530

    added pallete

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,8 @@ import 'package:surrealconvo/home.dart';
 void main() {
   runApp(
     const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Surreal Convo",
       home: Scaffold(
         body: Center(
           child: HomePage(),
